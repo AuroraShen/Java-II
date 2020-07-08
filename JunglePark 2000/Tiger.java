be8391389a304f6ad326ecfa04b94371dac537af
AuroraShen <47667931+AuroraShen@users.noreply.github.com>
2020-07-07 21:50:02 -0500

    Update Tiger.java

--- JunglePark 2000/Tiger.java
+++ JunglePark 2000/Tiger.java
@@ -1,41 +1,13 @@
 //////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
 //
 // Title:           Jungle Park 2000
-// Files:           JunglePark.java, ParkGUI.java, Animal.java, Tiger.java, Deer.java, 
-//                  AddAnimalButton.java, ClearButton.java, and JungkeParkTests.java
-// Course:          CS 300, Fall 2018
-//
-// Author:          Ruoxi Shen
-// Email:           rshen27@wisc.edu
+// Author:          rshen27@wisc.edu
 // Lecturer's Name: Gary Dahl
-//
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
-//
-// Partner Name:    (name of your pair programming partner)
-// Partner Email:   (email address of your programming partner)
-// Partner Lecturer's Name: (name of your partner's lecturer)
-// 
-// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
-//   ___ Write-up states that pair programming is allowed for this assignment.
-//   ___ We have both read and understand the course Pair Programming Policy.
-//   ___ We have registered our team prior to the team registration deadline.
-//
-///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
-//
-// Students who get help from sources other than their partner must fully 
-// acknowledge and credit those sources of help here.  Instructors and TAs do 
-// not need to be credited here, but tutors, friends, relatives, room mates, 
-// strangers, and others do.  If you received no outside help from either type
-//  of source, then please explicitly indicate NONE.
-//
-// Persons:         NONE
-// Online Sources:  NONE
 //
 /////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
 
 /**
  * This class represents a Tiger in the JunglePark application
- *
  */
 public class Tiger extends Animal {
   private static final int SCAN_RANGE = 100; // range dimension for scanning the neighborhood for food
