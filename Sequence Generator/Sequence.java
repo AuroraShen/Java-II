148d32b80fdd97a132d5fcc9485b812f9136e9a7
AuroraShen <47667931+AuroraShen@users.noreply.github.com>
2020-07-07 22:31:09 -0500

    Update Sequence.java

--- Sequence Generator/Sequence.java
+++ Sequence Generator/Sequence.java
@@ -1,38 +1,10 @@
 /////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
 //
-// Title: Numeric Sequence Generator
-// Files: Sequence.java, GeometricSequenceGenerator.java, FibonacciSequenceGenerator.java, 
-// DigitProductSequenceGenerator.java, and SequenceGeneratorTests.java
-// Course:CS 300, Fall 2018
-//
-// Author:     Ruoxi Shen
-// Email:      rshen27@wisc.edu
+// Title:      Numeric Sequence Generator
+// Author:     rshen27@wisc.edu
 // Lecturer's Name: Gary Dahl
 //
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
-//
-// Partner Name: N/A
-// Partner Email: N/A
-// Partner Lecturer's Name: N/A
-//
-// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
-// ___ Write-up states that pair programming is allowed for this assignment.
-// ___ We have both read and understand the course Pair Programming Policy.
-// ___ We have registered our team prior to the team registration deadline.
-//
-///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
-//
-// Students who get help from sources other than their partner must fully
-// acknowledge and credit those sources of help here. Instructors and TAs do
-// not need to be credited here, but tutors, friends, relatives, room mates,
-// strangers, and others do. If you received no outside help from either type
-// of source, then please explicitly indicate NONE.
-//
-// Persons: N/A
-// Online Sources: N/A
-//
 /////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
-
 
 import java.util.Iterator;
 import java.util.Scanner;
@@ -41,8 +13,6 @@ import java.util.Scanner;
  * This class represents a sequence generator for a numerical progression
  * This sequence generator can generate arithmetic, geometric, Fibonacci, 
  * and digit product sequences
- * 
- * @author mouna & Shen
  */
 
 public class Sequence implements Iterable<Integer> {
